8d04716a98e999012d555cf93af36de92ea83819
bkfwebdev <bryant.feld@gmail.com>
2017-07-29 15:04:23 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,107 @@
+const data = [
+  {
+    id: 0,
+    name: "Bryant K. Feld",
+    role: "Organizer",
+    avatar: "https://secure.meetupstatic.com/photos/member/2/b/8/e/member_263531150.jpeg",
+    attended: true
+  },
+  {
+    id: 1,
+    name: "Roy Racer",
+    role: "Presenter",
+    avatar: "https://secure.meetupstatic.com/photos/member/6/3/6/e/highres_243445454.jpeg",
+    attended: true
+  },
+  {
+    id: 2,
+    name: "Mike Fox",
+    role: "Attendee",
+    avatar: "https://secure.meetupstatic.com/photos/member/5/f/c/1/highres_8724513.jpeg",
+    attended: false
+  },
+  {
+    id: 3,
+    name: "Vikki Walker",
+    role: "Attendee",
+    avatar: "https://secure.meetupstatic.com/photos/member/8/8/f/e/member_269015070.jpeg",
+    attended: false
+  },
+  {
+    id: 4,
+    name: "Phillip Kazanjian",
+    role: "Attendee",
+    avatar: "https://secure.meetupstatic.com/photos/member/1/7/e/3/member_268806115.jpeg",
+    attended: false
+  },
+  {
+    id: 5,
+    name: "Joshua Henderson",
+    role: "Attendee",
+    avatar: "https://secure.meetupstatic.com/photos/member/1/f/5/e/member_256388030.jpeg",
+    attended: false
+  },
+  {
+    id: 6,
+    name: "Freddy",
+    role: "Attendee",
+    avatar: "https://secure.meetupstatic.com/photos/member/e/0/b/2/member_162957522.jpeg",
+    attended: false
+  },
+  {
+    id: 7,
+    name: "Bob Miller",
+    role: "Attendee",
+    avatar: "https://secure.meetupstatic.com/photos/member/3/e/f/5/highres_264736117.jpeg",
+    attended: false
+  },
+  {
+    id: 8,
+    name: "Z",
+    role: "Attendee",
+    avatar: "https://secure.meetupstatic.com/photos/member/b/7/5/d/member_14506941.jpeg",
+    attended: false
+  },
+  {
+    id: 9,
+    name: "Jin Soo Lim",
+    role: "Attendee",
+    avatar: "https://secure.meetupstatic.com/photos/member/9/e/d/3/member_269080659.jpeg",
+    attended: false
+  },
+  {
+    id: 10,
+    name: "Gary",
+    role: "Attendee",
+    avatar: "https://secure.meetupstatic.com/photos/member/6/1/b/e/member_178465022.jpeg",
+    attended: false
+  },
+  {
+    id: 11,
+    name: "Frank",
+    role: "Attendee",
+    avatar: "https://secure.meetupstatic.com/photos/member/1/d/7/b/member_8407547.jpeg",
+    attended: false
+  },
+  {
+    id: 12,
+    name: "Richard Dewgard",
+    role: "Attendee",
+    avatar: "https://secure.meetupstatic.com/photos/member/1/7/5/f/member_268325983.jpeg",
+    attended: false
+  },
+  {
+    id: 13,
+    name: "David Koch",
+    role: "Attendee",
+    avatar: "https://secure.meetupstatic.com/photos/member/b/0/6/2/member_4065154.jpeg",
+    attended: false
+  },
+  {
+    id: 14,
+    name: "N.D.",
+    role: "Attendee",
+    avatar: "https://secure.meetupstatic.com/s/img/2982428616572973604/noPhoto_80.gif",
+    attended: false
+  }
+];
